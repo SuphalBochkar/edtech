28766b11faa65d382d895f52c3e93e50a3736895
Suphal Bochkar <suphalbochkar@gmail.com>
2024-11-10 13:36:37 +0530

    Feature: Footer

--- src/app/(pages)/test/page.tsx
+++ src/app/(pages)/test/page.tsx
@@ -4,26 +4,24 @@ import { useSession } from "next-auth/react";
 import AllTests from "@/components/Tests/AllTests";
 import WelcomeMsg from "@/components/WelcomeMsg";
 import Blobs from "@/components/Blobs";
+import Footer from "@/components/Footer";
 
 const Page = () => {
   const { data } = useSession();
   const user = data?.user;
 
   return (
-    <div className="flex flex-col relative overflow-hidden">
+    <div className="flex flex-col relative overflow-hidden justify-between max-h-screen">
       <Blobs />
       <div className="flex flex-col align-middle justify-center items-center content-center">
         <WelcomeMsg name={user?.name || "User"} isPaid={user?.paid || false} />
         <AllTests />
       </div>
-
-      {user && user?.email === process.env.NEXT_PUBLIC_MY_EMAIL! && (
-        <div className="w-full">
-          <p className="text-3xl text-center">
-            Business Name: {process.env.NEXT_PUBLIC_MY_NAME!}
-          </p>
-        </div>
-      )}
+      <div>
+        {user && user?.email === process.env.NEXT_PUBLIC_MY_EMAIL! && (
+          <Footer />
+        )}
+      </div>
     </div>
   );
 };
