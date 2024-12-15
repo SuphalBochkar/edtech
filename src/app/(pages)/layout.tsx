a4af9932d601ddb631b7a85f8d8a1a97a586210b
Suphal Bochkar <suphalbochkar@gmail.com>
2024-12-16 01:19:05 +0530

    maintenance update

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -5,8 +5,7 @@ import "./globals.css";
 import Providers from "@/providers/providers";
 import { Lato } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
-// import Maintenance from "@/components/Maintenance";
-// import Script from "next/script";
+import Maintenance from "@/components/Main/Maintenance";
 
 export const metadata: Metadata = {
   title: "Finish66 EdTech",
@@ -19,22 +18,18 @@ const lato = Lato({
   weight: ["100", "300", "400", "700", "900"],
 });
 
-export default async function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default async function RootLayout() {
+// { children }: Readonly<{children: React.ReactNode}>
   return (
     <html lang="en" suppressHydrationWarning>
       <link rel="icon" href="./icon.svg" type="image/svg+xml" />
       <body className={`${lato.className} bg-background text-foreground`}>
-        <Providers>{children}</Providers>
-        {/* <Providers>
+        {/* <Providers>{children}</Providers> */}
+        <Providers>
           <Maintenance />
-        </Providers> */}
+        </Providers>
         <Analytics />
       </body>
-      {/* <Script src="https://checkout.razorpay.com/v1/checkout.js" /> */}
     </html>
   );
 }
--- src/components/Main/Maintenance.tsx
+++ src/components/Main/Maintenance.tsx
@@ -1,8 +1,18 @@
 const Maintenance = () => {
   return (
-    <div>
-      <div>Server Crashed due to traffic 🤣, will be back soon today</div>
-      <div>Under Maintenance</div>
+    <div className="flex justify-center items-center min-h-screen p-4">
+      <div className="p-8 rounded-lg shadow-lg text-center w-full max-w-lg">
+        <h1 className="text-3xl font-bold mb-6 leading-tight">
+          We are currently updating the tests and course levels.
+        </h1>
+        <p className="text-xl mb-4">
+          Rest assured, answers will be provided promptly once the updates are
+          complete.
+        </p>
+        <p className="text-lg">
+          Thank you for your patience and understanding.
+        </p>
+      </div>
     </div>
   );
 };
